486f50b5a0bde0780479840e2f58d09745580649
Nadimmo <nadimmostafa334@gamil.com>
2026-03-23 22:20:29 +0600

    update vercel

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect(); 
+    // await client.connect(); 
 
     const db = client.db("visa-processingDB");
     const userCollection = db.collection("usersInfo");
